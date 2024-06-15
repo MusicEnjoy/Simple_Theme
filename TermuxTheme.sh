cda6e547eaeef479f7e41af9dcbb1291fc225bac
MusicEnjoy <115658647+MusicEnjoy@users.noreply.github.com>
2024-06-15 17:37:37 +0700

    Add files via upload

---
+++ TermuxTheme.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Coded by HαρρყDҽʋιL
+# Created since on 10/02/2021 made in Nigeria by HαρρყDҽʋιL.
+# If You Take Money For This Little Bash Script Please Look At Your Self Again And Shame on You.
+# visit our website https://bajetech.org
+clear
+sleep 0.5
+echo ""
+echo "  [---VISIT https://bajetech.org TO LEARN MORE---]"|lolcat
+echo "    _____   _____ _   _ _____ __  __ _____"|lolcat
+echo "   |_   _| |_   _| | | | ____|  \/  | ____|"|lolcat
+echo "     | |    | | | |_| |  _| | |\/| |  _|"  |lolcat
+echo "     | |     | | |  _  | |___| |  | | |___" |lolcat
+echo "     |_|     |_| |_| |_|_____|_|  |_|_____| V1.5"|lolcat
+echo "     [CREATED BY: HαρρყDҽʋιL]"|lolcat
+sleep 0.5
+echo ""
+echo "You Are About To Switch Termux To Colorfull Mode."|lolcat
+read -p 'Press Enter To Continue Or Press CTRL + Z To Cancel'
+echo ""
+echo ""
+sleep 0.5
+echo "Ok Your Termux Theme Will Change Now"|lolcat
+sleep 2.0
+echo " Please Wait For 10 Seconds To Make The Changes..."|lolcat
+sleep 2.0
+cd
+cd /$HOME
+rm //data/data/com.termux/files/usr/etc/bash.bashrc
+cd
+mv /data/data/com.termux/files/home/TermuxTheme/code/bash.bashrc //data/data/com.termux/files/usr/etc/bash.bashrc
+sleep 8.0
+echo "Completed, Your Termux Now Looking Awesome"|lolcat
+sleep 1.0
+echo ""
+echo ""
+echo "Please exit from the termux and open it again to see the changes."|lolcat
+
+echo ""
+echo ""
+
+echo "Then you type exit 3x"|lolcat
+
+echo ""
+echo ""
+
+
+
+echo "VISIT https://bajetech.org TO LEARN MORE."
